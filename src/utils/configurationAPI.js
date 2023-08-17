0dd20e9a838b46d2d1c52d042e49557fc15f2f35
AnibalN25 <aniballaura25@gmail.com>
2023-08-17 09:26:14 -0400

    Cambio de dominio del servidor

--- src/utils/configurationAPI.js
+++ src/utils/configurationAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://localhost:3100/',
+    baseURL: 'https://management-schedule-server.vercel.app/',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
