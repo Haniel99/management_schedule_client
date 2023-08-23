70df80197102e3063e3bd2d8db9d54e687bec2de
Haniel <104791894+Haniel99@users.noreply.github.com>
2023-08-23 14:20:57 -0400

    Update configurationAPI.js
    Cambio de la api

--- src/utils/configurationAPI.js
+++ src/utils/configurationAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://management-schedule-server.vercel.app/',
+    baseURL: 'https://server-ts-omega.vercel.app/',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
