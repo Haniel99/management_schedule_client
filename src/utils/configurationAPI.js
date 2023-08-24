a402ae531da9a706a64b9a4d75434ee3e54dde5a
Lucas-Vfo <fabian.flores.oyarce@alumnos.uta.cl>
2023-08-24 08:36:58 -0400

    Update configuration API

--- src/utils/configurationAPI.js
+++ src/utils/configurationAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://server-ts-omega.vercel.app/',
+    baseURL: 'server-ts-six.vercel.app',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
