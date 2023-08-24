6780a6e1f8d155200fa52f6995054a37f298a480
Lucas-Vfo <fabian.flores.oyarce@alumnos.uta.cl>
2023-08-24 08:40:39 -0400

    configuration API

--- src/utils/configurationAPI.js
+++ src/utils/configurationAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'server-ts-six.vercel.app',
+    baseURL: 'https://server-ts-six.vercel.app/',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
