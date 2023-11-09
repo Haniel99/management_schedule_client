ee7e40451fc38b38fa4885d2ed8a3e58c8f13c8b
Lucas-Vfo <72819147+Lucas-Vfo@users.noreply.github.com>
2023-11-08 23:01:02 -0300

    ads

--- src/pages/private/Professor/component/ProfessorSetting.jsx
+++ src/pages/private/Professor/component/ProfessorSetting.jsx
@@ -114,20 +114,20 @@ const ProfessorSetting = ({ onSave, onCancel }) => {
 
                         <div class="grid gap-6 mb-6 lg:grid-cols-2">
                             <div>
-                            <button 
-                            type="button"
-                            onClick={handleSave}
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Guardar
-                            </button>
+                                <button 
+                                type="button"
+                                onClick={handleSave}
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Guardar
+                                </button>
                             </div>
                             <div>
-                            <button 
-                            type="button"
-                            onClick={onCancel}
-                            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                            Cancelar
-                            </button>
+                                <button 
+                                type="button"
+                                onClick={onCancel}
+                                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Cancelar
+                                </button>
                             </div>
                         </div>
                     </div>
